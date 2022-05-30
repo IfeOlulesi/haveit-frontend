3d7abfac9d4ee52768bb70d85766a05b189166de
IfeOlulesi <ifeolulesi@gmail.com>
2022-05-30 23:25:59 +0100

    HF-12 Added Raleway font globally

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+import "./fonts/Raleway-Bold.ttf";
+import "./fonts/Raleway-SemiBold.ttf";
+import "./fonts/Raleway-Regular.ttf";
+import "./fonts/Raleway-Light.ttf";
+import "./fonts/Raleway-Medium.ttf";
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
