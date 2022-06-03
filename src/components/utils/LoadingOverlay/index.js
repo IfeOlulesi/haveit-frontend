fb0c3f50f285ecd3cd81ff83c09b47020d1216ac
IfeOlulesi <ifeolulesi@gmail.com>
2022-06-03 15:00:51 +0100

    Changed loading overlay

--- src/components/utils/LoadingOverlay/index.js
+++ src/components/utils/LoadingOverlay/index.js
@@ -1,8 +1,34 @@
 import React from "react";
+import Backdrop from '@material-ui/core/Backdrop';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
-const LoadingOverlay = () => {
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  loaderContainer: {
+    display: "flex",
+    flexDirection: "column", 
+  },
+  backdrop: {
+    zIndex: theme.zIndex.drawer + 1,
+    backgroundColor: '#f2f2f2',
+    // color: "#FFF",
+  },
+}))
+
+const LoadingOverlay = ({open, setOpen}) => {
+  const classes = useStyles();
+  // const [open, setOpen] = React.useState(false);
+
   return (
-    <p>LOADING....</p>
+    <div>
+      <Backdrop open={true} className={classes.backdrop}>
+        <div className={classes.loaderContainer}>
+          <CircularProgress color="primary" />
+          <div className="font-rb-medium">Loading...</div>
+        </div>
+      </Backdrop>
+    </div>
   )
 }
 
